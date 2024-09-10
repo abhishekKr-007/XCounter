f76c8d2340e1fc5b1de65746d3f06707a46a00c3
abhishekKr-007 <abhishekkumar314328@gmail.com>
2024-09-11 03:49:07 +0530

    x_counter

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Counter App</h1>
+        <p> Count: {count}</p>
+      </div>
+      <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+      <button onClick={() => setCount((count) => count - 1)}>Decrement</button>
+    </>
+  );
+}
+
+export default App;
